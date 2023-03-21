e4a35a9828b9c18ef284b249525f726e7ac5314a
Vivek0218 <105193786+Vivek0218@users.noreply.github.com>
2023-03-21 12:33:32 +0530

    Add Var files

---
+++ Modules/Resource_Group/var.tf
@@ -0,0 +1,9 @@
+variable "rgname" {
+    type = string
+    description = "Name for the Resource-Group"
+}
+
+variable "rgregion" {
+  type = string
+  description = "Location for the Resource-Group"
+}
