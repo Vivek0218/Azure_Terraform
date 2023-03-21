b16d28a9c3e68211c239b4fe399ecca46d75d40e
Vivek0218 <105193786+Vivek0218@users.noreply.github.com>
2023-03-21 12:34:07 +0530

    Delete var.tf

--- var.tf
+++
@@ -1,9 +0,0 @@
-variable "rgname" {
-    type = string
-    description = "Name for the Resource-Group"
-}
-
-variable "rgregion" {
-  type = string
-  description = "Location for the Resource-Group"
-}
