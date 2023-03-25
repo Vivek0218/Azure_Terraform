87f1052b298bcad0b31564a9ad56e7753d6b7a03
Vivek Padale <vivekpadale18@gmail.com>
2023-03-25 21:10:56 +0530

    Update Outpu

--- Modules/Virtual_Network/output.tf
+++ Modules/Virtual_Network/output.tf
@@ -6,10 +6,6 @@ output "Virtual-Network-Address-Space" {
   value = azurerm_virtual_network.vnet001.address_space
 }
 
-output "Subnet-Name" {
-    value = azurerm_virtual_network.vnet001.subnet.name
-}
-
-output "Subnet-Address-Prefix" {
-  value = azurerm_virtual_network.vnet001.subnet.address_prefix
+output "Subnet" {
+    value = azurerm_virtual_network.vnet001.subnet
 }
