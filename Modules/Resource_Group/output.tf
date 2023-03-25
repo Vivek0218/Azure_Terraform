ec26fb7df947c6e0d96f3f3f44e4ab5ca74b4636
Vivek Padale <vivekpadale18@gmail.com>
2023-03-25 20:12:40 +0530

    Update RG-Output.tf

--- Modules/Resource_Group/output.tf
+++ Modules/Resource_Group/output.tf
@@ -1,7 +1,7 @@
-output "Resource_Group_Name-" {
+output "Resource_Group_Name" {
 value = azurerm_resource_group.rg.name
 }
   
-output "Resource_Group_Location-" {
+output "Resource_Group_Location" {
   value = azurerm_resource_group.rg.location
 }
